6c1733fa898047c7dce6dd92a8dc36a7ab8366b8
Muntasir00 <muntasir690000@gmail.com>
2022-07-08 00:01:43 +0600

    Add class based view for showing products

--- django-coding-test/src/product/views/product.py
+++ django-coding-test/src/product/views/product.py
@@ -1,6 +1,8 @@
 from django.views import generic
+from product.models import Product
 
 from product.models import Variant
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 class CreateProductView(generic.TemplateView):
@@ -12,3 +14,30 @@ class CreateProductView(generic.TemplateView):
         context['product'] = True
         context['variants'] = list(variants.all())
         return context
+
+
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/list.html'
+    context_object_name = 'products'
+    paginate_by = 2
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        products = self.get_queryset()
+        page = self.request.GET.get('page')
+        paginator = Paginator(products, self.paginate_by)
+        try:
+            products = paginator.page(page)
+        except PageNotAnInteger:
+            products = paginator.page(1)
+        except EmptyPage:
+            products = paginator.page(paginator.num_pages)
+        context['products'] = products
+        return context
+
+
+
+
+
+ 
